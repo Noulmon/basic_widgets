de329257b4b6c38cb5c6f98228e90e4107028b99
Noulmon <noulmonkc@gmail.com>
2019-02-23 23:05:54 +0530

    Added rows, colums, expanded widgets

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,24 +5,146 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
         child: Container(
-          alignment: Alignment.center,
-          color: Colors.green,
+            alignment: Alignment.center,
+            color: Colors.green,
 //          width: 200.0,
 //          height: 100.0,
 //          margin: EdgeInsets.only(left: 30.0, bottom: 30.0),
-          padding: EdgeInsets.all(20.0),
-          child: Text(
-            "Flight",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-              decoration: TextDecoration.none,
-              fontFamily: 'Righteous',
-              fontWeight: FontWeight.w500,
-              fontStyle: FontStyle.italic,
-              fontSize: 75.0,
-              color: Colors.white,
-            ), ),
-        )
-    );
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Google",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Silicon Valley, USA",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Google",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Silicon Valley, USA",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Google",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Silicon Valley, USA",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        "Google",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 40.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Text(
+                        "Silicon Valley, USA",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                          decoration: TextDecoration.none,
+                          fontFamily: 'Righteous',
+                          fontWeight: FontWeight.w500,
+                          fontStyle: FontStyle.italic,
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )));
   }
 }
